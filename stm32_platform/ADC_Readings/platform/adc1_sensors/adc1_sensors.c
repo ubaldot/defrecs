390b4c7aa2440546c3e5b777af55e17a888d91a7
Ubaldo Tiberi <ubaldo.tiberi@volvo.com>
2024-02-21 11:59:40 +0100

    .

--- stm32_platform/ADC_Readings/platform/adc1_sensors/adc1_sensors.c
+++ stm32_platform/ADC_Readings/platform/adc1_sensors/adc1_sensors.c
@@ -18,15 +18,18 @@
 #include "semphr.h"
 #include <string.h>
 
-// Signals
-static float adc1_pv_pin_voltage;
-static SemaphoreHandle_t mutex_adc1_pv_pin_voltage;
-
 // Double check the numbers with hadc1.Init values!!!
 const uint8_t ADC1_RESOLUTION_BITS = 10;
 const uint8_t NUM_CHANNELS = 2;
 const float ADC1_PINS_VOLTAGE = 3300.0F;
 float ADC1_RESOLUTION;
+
+// Signals
+static float adc1_pv_pin_voltage;
+static SemaphoreHandle_t mutex_adc1_pv_pin_voltage;
+
+static float adc1_tempsens_pin_voltage;
+static SemaphoreHandle_t mutex_adc1_tempsens_pin_voltage;
 
 // Publish
 static void publish_adc1_pv_pin_voltage(const float *pVoltage) {
@@ -34,6 +37,14 @@ static void publish_adc1_pv_pin_voltage(const float *pVoltage) {
       pdTRUE) {
     memcpy(&adc1_pv_pin_voltage, pVoltage, sizeof(*pVoltage));
     xSemaphoreGive(mutex_adc1_pv_pin_voltage);
+  }
+}
+
+static void publish_adc1_tempsens_pin_voltage(const float *pVoltage) {
+  if (xSemaphoreTake(mutex_adc1_tempsens_pin_voltage,
+                     100 / portTICK_PERIOD_MS) == pdTRUE) {
+    memcpy(&adc1_tempsens_pin_voltage, pVoltage, sizeof(*pVoltage));
+    xSemaphoreGive(mutex_adc1_tempsens_pin_voltage);
   }
 }
 
@@ -47,6 +58,15 @@ void subscribe_adc1_pv_pin_voltage(float *pVoltage) {
   }
 }
 
+void subscribe_adc1_tempsens_pin_voltage(float *pVoltage) {
+  // Returns a copy of the output
+  if (xSemaphoreTake(mutex_adc1_tempsens_pin_voltage,
+                     100 / portTICK_PERIOD_MS) == pdTRUE) {
+    memcpy(pVoltage, &adc1_tempsens_pin_voltage, sizeof(*pVoltage));
+    xSemaphoreGive(mutex_adc1_tempsens_pin_voltage);
+  }
+}
+
 void adc1_sensors_init() {
   ADC1_RESOLUTION =
       ADC1_PINS_VOLTAGE / (float)(1 << ADC1_RESOLUTION_BITS); // [mV]
@@ -56,6 +76,7 @@ void adc1_sensors_init() {
 
   // Mutex for protecting variables
   mutex_adc1_pv_pin_voltage = xSemaphoreCreateMutex();
+  mutex_adc1_tempsens_pin_voltage = xSemaphoreCreateMutex();
 }
 
 void adc1_sensors_step(enum WhoIsCalling caller) {
@@ -65,11 +86,15 @@ void adc1_sensors_step(enum WhoIsCalling caller) {
 
   HAL_ADC_Start_DMA(&hadc1, analog_read, NUM_CHANNELS);
   ulTaskNotifyTake(pdTRUE, portMAX_DELAY); // Wait for ADC
-                                                    // conversion end
+                                           // conversion end
   HAL_ADC_Stop_DMA(&hadc1);
 
   // Process and publish
   float pv_pin_voltage;
   pv_pin_voltage = ADC1_RESOLUTION * (float)analog_read[0] / 1000.0F;
   publish_adc1_pv_pin_voltage(&pv_pin_voltage);
+
+  float tempsens_pin_voltage;
+  pv_pin_voltage = ADC1_RESOLUTION * (float)analog_read[1] / 1000.0F;
+  publish_adc1_tempsens_pin_voltage(&tempsens_pin_voltage);
 }
