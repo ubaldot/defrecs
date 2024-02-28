ca7d2ab059416b28e11d10c8ce2442657c0ae900
ubaldot <ubaldo.tiberi@gmail.com>
2024-02-28 21:16:53 +0100

    Updated folders names

--- stm32f446_platform/Hello_World/platform/digital_out/digital_out.h
+++
@@ -1,9 +0,0 @@
-#ifndef DIGITAL_OUT_H
-#define DIGITAL_OUT_H
-
-#include "application_setup.h"
-
-void digital_out_init(void);
-void digital_out_step(enum WhoIsCalling caller);
-
-#endif /* DIGITAL_OUT_H */
