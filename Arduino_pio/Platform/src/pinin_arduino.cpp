e4c167fca9912e365d38f2de16b2e9731b6f2192
ubaldot <ubaldo.tiberi@gmail.com>
2024-03-06 08:04:36 +0100

    Removed Ardino_pio

--- Arduino_pio/Platform/src/pinin_arduino.cpp
+++
@@ -1,7 +0,0 @@
-#include "pinin_arduino.h"
-#include "hw_config_arduino.h"
-#include <Arduino.h>
-
-uint16_t pinin_pv(void) { return analogRead(PV); }
-
-uint16_t pinin_pv1(void) { return analogRead(PV2); }
--- Arduino_pio/Platform/src/pinin_arduino.h
+++
@@ -1,9 +0,0 @@
-#ifndef IN_FUNCTIONS_H
-#define IN_FUNCTIONS_H
-
-#include <Arduino.h>
-
-uint16_t pinin_pv(void);
-uint16_t pinin_pv1(void);
-
-#endif /* IN_FUNCTIONS_H */
