51eb339bf9b331e3c552af33631686b42629316f
Ubaldo Tiberi <ubaldo.tiberi@gmail.com>
2024-05-06 19:33:33 +0200

    .

--- stm32f4x/gdb_stuff/openocd_stm32f4x_stlink.sh
+++ stm32f4x/gdb_stuff/openocd_stm32f4x_stlink.sh
@@ -1,5 +1,6 @@
-#!bin/bash
+#!/bin/bash
 
+openocd_path="/opt/homebrew/bin"
 openocd_scripts_path="/opt/homebrew/Cellar/open-ocd/0.12.0_1/share/openocd/scripts"
 
 probes_path=$openocd_scripts_path/interface
@@ -8,4 +9,4 @@ probe="stlink.cfg"
 boards_path=$openocd_scripts_path/target
 board="stm32f4x.cfg"
 
-openocd -f "$probes_path/$probe" -f "$boards_path/$board"
+"$openocd_path"/openocd -f "$probes_path/$probe" -f "$boards_path/$board"
