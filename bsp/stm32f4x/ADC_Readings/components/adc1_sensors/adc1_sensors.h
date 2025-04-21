2c3dac39b5d650b1bbe6e6d0aafd176845b83bed
Ubaldo Tiberi <ubaldo.tiberi@gmail.com>
2025-04-21 19:03:53 +0200

    .

---
+++ bsp/stm32f4x/ADC_Readings/components/adc1_sensors/adc1_sensors.h
@@ -0,0 +1,12 @@
+#ifndef ADC1_SENSORS_H
+#define ADC1_SENSORS_H
+
+#include "application_setup.h"
+
+void adc1_sensors_init(void);
+void adc1_sensors_step(enum WhoIsCalling);
+
+void subscribe_adc1_ch0_pin_voltage(float*);
+void subscribe_adc1_ch1_pin_voltage(float *);
+
+#endif /* ADC1_SENSORS_H */
---
+++ bsp/stm32f4x/utils/ftoa.h
@@ -0,0 +1,5 @@
+#ifndef FTOA_H
+#define FTOA_H
+char *ftoa(double f, char *pBuf, int precision);
+
+#endif /* FTOA_H*/
